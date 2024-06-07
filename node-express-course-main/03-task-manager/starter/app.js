2a7031b8c757fb8ca2fef83c4c4b2fa3465d82a6
IAMOTAS <niiotasalokome@gmail.com>
2024-06-07 05:19:49 +0000

    7th-june-2024

--- node-express-course-main/03-task-manager/starter/app.js
+++ node-express-course-main/03-task-manager/starter/app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+app.listen( "3000",()=>{
+    console.log("Server is Up and running");
+})
+
+/*
+
+const PORT = 3000;
+app.listen(PORT,console.log(`Server is listening on ${PORT}`));
+
+*/
+
